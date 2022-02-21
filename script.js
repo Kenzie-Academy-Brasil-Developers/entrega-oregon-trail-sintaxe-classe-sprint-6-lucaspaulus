22477a474763e9bcb5d60995f0a36f4ea2d92af4
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-02-20 21:59:15 -0300

    adicionei os setters e getters que faltava

--- script.js
+++ script.js
@@ -1,55 +1,101 @@
 class Traveler {
     constructor(name) {
-        this.name = name
-        this.food = 1
-        this.isHealthy = true
+        this._name = name
+        this._food = 1
+        this._isHealthy = true
+    }
+    set name(characterName){
+        this._name = characterName
+    
     }
 
-   hunt() {
-       // sitting code here
+    get name(){
+        return this._name
+    }
+
+    set food(qtdFood){
+        this._food = qtdFood
+    }    
+
+    get food(){
+        return this._food
+    }
+
+    set isHealthy(characterHealthy){
+        this._isHealthy = characterHealthy
+    }
+
+    get isHealthy(){
+        return this._isHealthy
+    }
+   
+    hunt() {
+       // sitting code herev
        this.food += 2
-   }
-   eat() {
+    }
+    eat() {
        // fetching code here
        if(this.food > 0){
             this.food--
        }else{
             this.isHealthy = false   
        }
-   }
+    }
 }
 
 class Wagon{
     constructor(capacity){
-        this.capacity = capacity
-        this.passengers = []
+        this._capacity = capacity
+        this._passengers = []
     }
+
+    set capacity(characterCapacity){
+        this._capacity = characterCapacity
+    }
+
+    get capacity(){
+        return this._capacity
+    }
+
+    set passengers(qtdPassengers){
+        this._passengers = qtdPassengers
+    }
+    
+    get passengers(){
+        return this._passengers
+    }
+    
     getAvailableSeatCount(){
-        return this.capacity - this.passengers.length
+        return this._capacity - this._passengers.length
     }
+
     join(Traveler){
         if(this.getAvailableSeatCount() > 0){
-            this.passengers.push(Traveler)
+            this._passengers.push(Traveler)
         }
     }
+
     shouldQuarantine(){
-        for(let i = 0; i < this.passengers.length; i++){
-            if(!this.passengers[i].isHealthy){
+        for(let i = 0; i < this._passengers.length; i++){
+            if(!this._passengers[i].isHealthy){
                 return true
             }
         }
         return false
     }
+
     totalFood(){
         let totalFoods = 0
-        for(let i = 0; i < this.passengers.length; i++){
-            totalFoods += this.passengers[i].food 
+        for(let i = 0; i < this._passengers.length; i++){
+            totalFoods += this._passengers[i].food 
         }
+
         return totalFoods
     }
 }
 
 /*****************************TESTA O CÓDIGO***************************/ 
+
 // Criar uma carroça que comporta 2 pessoas
 let wagon = new Wagon(2);
 // Criar três viajantes
